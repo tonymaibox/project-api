a75a9e5c4632b46d6d7a3cdb6d7e88409de3fd24
Tony Mai <tonymaibox@yahoo.com>
2016-10-04 13:42:26 -0400

    commented out authentications

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,17 +1,17 @@
 module Api
 	module V1
 		class SessionsController < ApplicationController
-			skip_before_action :authenticate
+			# skip_before_action :authenticate
 
-			  def create
-			  	# binding.pry
-			    user = User.find_by(email: params[:email])
-			    if user.authenticate(params[:password])
-			      jwt = Auth.issue({id: user.id})
-			      render json: {jwt: jwt}
-			    else
-			    end
-			  end
+			  # def create
+			  # 	# binding.pry
+			  #   user = User.find_by(email: params[:email])
+			  #   if user.authenticate(params[:password])
+			  #     jwt = Auth.issue({id: user.id})
+			  #     render json: {jwt: jwt}
+			  #   else
+			  #   end
+			  # end
 
 			  private
 			    def auth_params
