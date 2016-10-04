d74e65330f5697a70579842d4ae1f2274e538a4d
Schyler Hosler <schylerhosler@gmail.com>
2016-10-04 08:50:53 -0400

    This update will relocate controllers to appropriate '.../api/v1' path and update routes accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
-  resources :activities
-  resources :locations
-  resources :days
-  resources :itineraries
-  resources :users
+  # resources :activities
+  # resources :locations
+  # resources :days
+  # resources :itineraries
+  # resources :users
 
   namespace :api do
   	namespace :v1 do
   		post '/login', to: "sessions#create"
+      # We will likely need to limit the routes for the below resources:
+      resources :activities
+      resources :locations
+      resources :days
+      resources :itineraries
+      resources :users
   	end
   end
-  
 
-  
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
