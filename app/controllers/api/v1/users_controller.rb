a4a6960c51f1b549d6dc8d6d8f2a3183d7789a5b
Tony Mai <tonymaibox@yahoo.com>
2016-10-09 15:23:40 -0400

    users can be created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,26 @@
-class UsersController < ApplicationController
+module Api
+	module V1
+		class UsersController < ApplicationController
 
-  skip_before_action :authenticate
+		  skip_before_action :authenticate
 
-  def create
-    binding.pry
-    user = User.new
-  end
+		  def create
+		    # binding.pry
+		    user = User.new(user_params)
 
+		    if user.save
+		      render json: user
+		    else
+		      render json: user.errors, status: 500
+		    end
+		  end
+
+		  private
+
+		  def user_params
+		  	params.require(:user).permit(:username, :email, :first_name, :last_name, :password)
+		  end
+
+		end
+	end
 end
